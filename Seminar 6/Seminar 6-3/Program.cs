e48af71d98743106a0b74240009f7ecb16390191
hav1k <anton.andrianov.94@mail.ru>
2023-09-03 12:20:07 +0300

    update2

--- Seminar 6/Seminar 6-3/Program.cs
+++ Seminar 6/Seminar 6-3/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int fibbonacci1 = 0, fibbonacci2 = 1, fibbonacciSum;
+
+Console.Write(fibbonacci1 + " ");
+Console.Write(fibbonacci2 + " ");
+
+int count = 0;
+while (count < number - 2){
+fibbonacciSum = fibbonacci1 + fibbonacci2;
+fibbonacci1 = fibbonacci2;
+fibbonacci2 = fibbonacciSum;
+Console.Write(fibbonacciSum + " ");
+count ++;
+}
+
+// void Fib (int value){
+// int [] array = new int [value];
+// for (int i = 0; i < array.Length; i++)
+// {
+// if (i == 0){
+// array[i] = 0;
+// Console.Write(array[i] + " ");
+// }
+// else if(i == 1){
+// array[i] = 1;
+// Console.Write(array[i] + " ");
+// }
+// else{
+// array[i] = array[i-1]+ array[i-2];
+// Console.Write(array[i] + " ");
+// }
+// }
+// }
+// Fib(3);
--- Seminar 6/Seminar 6-4/Program.cs
+++ Seminar 6/Seminar 6-4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.Write("Введите десятичное число: ");
+int decimalNumber = int.Parse(Console.ReadLine());
+string binaryString = string.Empty;
+while (decimalNumber > 0)
+{
+int remainder = decimalNumber % 2;
+decimalNumber /= 2;
+binaryString = remainder.ToString() + binaryString;
+}
+Console.WriteLine($"Двоичное представление числа: {binaryString}");
