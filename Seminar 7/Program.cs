cc3c7abc679789fd59b55e368c00fa5bc38dc9bb
hav1k <anton.andrianov.94@mail.ru>
2023-09-05 21:30:19 +0300

    Sm_7

---
+++ Seminar 7/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+
+int[,] array = new int[4, 7];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(array);
---
+++ Seminar 7/Seminar 7-3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+//  элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int[,] array = new int[3, 4];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 20);
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+Console.WriteLine();
+
+int SumElements(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                result = result + array[i, j];
+            }
+        }      
+    }
+    return result;
+}
+int summa = SumElements(array);
+Console.WriteLine("Cумма элементов, находящихся на главной диагонали равна: " + summa);
+
+
+
