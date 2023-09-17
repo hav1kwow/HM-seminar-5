5afdaa37bfb44ea5132ba095ab6d73d7322aff5e
hav1k <anton.andrianov.94@mail.ru>
2023-09-17 11:58:58 +0300

    HM_9

--- samoproverka/Program.cs
+++ samoproverka/Program.cs
@@ -1,180 +1,231 @@
-﻿// // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// // // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-// int Exponentiation(int numberA, int numberB){
-//   int result = 1;
-//   for(int i=1; i <= numberB; i++){
-//     result = result * numberA;
-//   }
+// // int Exponentiation(int numberA, int numberB){
+// //   int result = 1;
+// //   for(int i=1; i <= numberB; i++){
+// //     result = result * numberA;
+// //   }
     
-//     return result;
-// }
+// //     return result;
+// // }
 
-//   Console.Write("Введите число A: ");
-//   int numberA = Convert.ToInt32(Console.ReadLine());
-//   Console.Write("Введите число B: ");
-//   int numberB = Convert.ToInt32(Console.ReadLine());
+// //   Console.Write("Введите число A: ");
+// //   int numberA = Convert.ToInt32(Console.ReadLine());
+// //   Console.Write("Введите число B: ");
+// //   int numberB = Convert.ToInt32(Console.ReadLine());
 
-//   int exponentiation = Exponentiation(numberA, numberB);
-//   Console.WriteLine("Ответ: " + exponentiation);
-
-
+// //   int exponentiation = Exponentiation(numberA, numberB);
+// //   Console.WriteLine("Ответ: " + exponentiation);
 
 
-// //  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 
-// Console.Write("Введите число N: ");
-// int numberN = Convert.ToInt32(Console.ReadLine());
+// // //  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-//   int SumNumber(int numberN){
+
+// // Console.Write("Введите число N: ");
+// // int numberN = Convert.ToInt32(Console.ReadLine());
+
+// //   int SumNumber(int numberN){
     
-//     int count = Convert.ToString(numberN).Length;
-//     int advance = 0;
-//     int result = 0;
+// //     int count = Convert.ToString(numberN).Length;
+// //     int advance = 0;
+// //     int result = 0;
 
-//     for (int i = 0; i < count; i++){
-//       advance = numberN - numberN % 10;
-//       result = result + (numberN - advance);
-//       numberN = numberN / 10;
-//     }
-//    return result;
-//   }
+// //     for (int i = 0; i < count; i++){
+// //       advance = numberN - numberN % 10;
+// //       result = result + (numberN - advance);
+// //       numberN = numberN / 10;
+// //     }
+// //    return result;
+// //   }
 
-// int sumNumber = SumNumber(numberN);
-// Console.WriteLine("Сумма цифр в числе: " + sumNumber);
-
-
-// // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-
-// int[] array = new int[8];
-// int[] FillArray(int[] array){
-//         for (int i = 0; i < array.Length; i++){
-//         array[i] = new Random().Next(-9,10);
-//     }
-//     Console.WriteLine(String.Join(" ", array));
-//     return array;   
-// }
-// int[] newArray = FillArray(array);
+// // int sumNumber = SumNumber(numberN);
+// // Console.WriteLine("Сумма цифр в числе: " + sumNumber);
 
 
-// using System;
+// // // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-// public class Answer
-// {
-//     static bool IsPalindrome(int number){
-//       // Введите свое решение ниже
-//         string number1 = number.ToString();
-// int count = number1.Length;
+// // int[] array = new int[8];
+// // int[] FillArray(int[] array){
+// //         for (int i = 0; i < array.Length; i++){
+// //         array[i] = new Random().Next(-9,10);
+// //     }
+// //     Console.WriteLine(String.Join(" ", array));
+// //     return array;   
+// // }
+// // int[] newArray = FillArray(array);
 
-// if (count >= 6){Console.WriteLine($"Число не пятизначное");
-//      return false;
-//   }
-//   else if(number1[0]==number1[4] && number1[1]==number1[3]){return true;
-//   }
 
-// else    return false;
-//     }
+// // using System;
+
+// // public class Answer
+// // {
+// //     static bool IsPalindrome(int number){
+// //       // Введите свое решение ниже
+// //         string number1 = number.ToString();
+// // int count = number1.Length;
+
+// // if (count >= 6){Console.WriteLine($"Число не пятизначное");
+// //      return false;
+// //   }
+// //   else if(number1[0]==number1[4] && number1[1]==number1[3]){return true;
+// //   }
+
+// // else    return false;
+// //     }
   
-//   // Не удаляйте и не меняйте метод Main! 
-//       static public void Main(string[] args) {
-//         int number;
+// //   // Не удаляйте и не меняйте метод Main! 
+// //       static public void Main(string[] args) {
+// //         int number;
 
-//         if (args.Length >= 1) {
-//             number = int.Parse(args[0]);
-//         } else {
-//            // Здесь вы можете поменять значения для отправки кода на Выполнение
-//             number = 64546;
-//         }
+// //         if (args.Length >= 1) {
+// //             number = int.Parse(args[0]);
+// //         } else {
+// //            // Здесь вы можете поменять значения для отправки кода на Выполнение
+// //             number = 64546;
+// //         }
 
-//         // Не удаляйте строки ниже
-//         bool result = IsPalindrome(number);
-//         System.Console.WriteLine($"{result}");
+// //         // Не удаляйте строки ниже
+// //         bool result = IsPalindrome(number);
+// //         System.Console.WriteLine($"{result}");
+// //     }
+// // }
+
+
+// // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// // 0, 7, 8, -2, -2 -> 2
+// // 1, -7, 567, 89, 223-> 3
+
+// Console.Write("Введите числа через запятую: ");
+// int[] numbers = StringToNum(Console.ReadLine());
+// PrintArray(numbers);
+// int sum = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > 0)
+//     {
+//         sum++;
 //     }
+// }
+// Console.WriteLine();
+// Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+// int[] StringToNum(string input)
+// {
+//     int count = 1;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ',')
+//         {
+//             count++;
+//         }
+//     }
+
+//     int[] numbers = new int [count];
+//     int index = 0;
+
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         string temp = "";
+
+//         while (input [i] != ',')
+//         {
+//         if(i != input.Length - 1)
+//         {
+//             temp += input [i].ToString();
+//             i++;
+//         }
+//         else
+//         {
+//             temp += input [i].ToString();
+//             break;
+//         }
+//         }
+//         numbers[index] = Convert.ToInt32(temp);
+//         index++;
+//     }
+//     return numbers;
 // }
 
 
-// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
-
-Console.Write("Введите числа через запятую: ");
-int[] numbers = StringToNum(Console.ReadLine());
-PrintArray(numbers);
-int sum = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > 0)
-    {
-        sum++;
-    }
-}
-Console.WriteLine();
-Console.WriteLine($"количество значений больше 0 = {sum}");
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.Write("]");
+// }
 
 
-int[] StringToNum(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-        {
-            count++;
-        }
-    }
+// //  Напишите программу, которая найдёт точку пересечения 
+// //  двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-    int[] numbers = new int [count];
-    int index = 0;
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите значение b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
 
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
-}
+// double x = (-b2 + b1)/(-k1 + k2);
+// double y = k2 * x + b2;
+
+// Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 
 
-void PrintArray(int[] array)
-{
-    Console.Write("[ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.Write("]");
-}
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.Clear();
+// int Prompt(string message)
+// {
+//   Console.Write(message);
+//   int result = Convert.ToInt32(Console.ReadLine());
+//   return result;
+// }
+
+// int Akkerman(int m, int n)
+// {
+//   if (m == 0) return n + 1;
+//   if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+//   else return Akkerman(m - 1, Akkerman(m, n - 1));
+// }
+
+// int m = Prompt("Input M: ");
+// int n = Prompt("Input N: ");
+
+// Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
 
-//  Напишите программу, которая найдёт точку пересечения 
-//  двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// int Prompt(string message)
+// {
+//   Console.Write(message);
+//   int result = Convert.ToInt32(Console.ReadLine());
+//   return result;
+// }
 
-b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// int SumOfElements(int n, int m)
+// {
+//   if (n == m) return n;
+//   else return SumOfElements(n + 1, m) + n;
+// }
 
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k1");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите значение b2");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k2");
-double k2 = Convert.ToInt32(Console.ReadLine());
+// int n = Prompt("Input N: ");
+// int m = Prompt("Input M: ");
 
-double x = (-b2 + b1)/(-k1 + k2);
-double y = k2 * x + b2;
-
-Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+// Console.WriteLine(SumOfElements(n, m));
